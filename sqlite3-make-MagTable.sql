e4974d4d993e981ea682ab0cf21882246cbdc3fe
Malavika Vijayendra Vasist <malavikavijayendravasist@5072954F.static.ziggozakelijk.nl>
2017-10-17 12:29:57 +0200

    tables

---
+++ sqlite3-make-MagTable.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS MagTable (Name varchar(10),
+			 Ra varchar(10),
+			 Dec varchar(10),
+		   	 B FLOAT, 
+			 R FLOAT,
+			 UNIQUE(Name),
+			PRIMARY KEY(Name));
+
+
+
+
+
---
+++ sqlite3-make-PhysTable.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS MagTable (Name varchar(10),
+			 Teff varchar(10),
+			 FeH FLOAT,
+			 UNIQUE(Name),
+			 PRIMARY KEY(Name));
+
+
+
+
+
